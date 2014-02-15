246c5c2f768e50694eac27dc6d417a5106d721f6
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2014-02-15 18:41:26 +0000

    Use callbacks to increase accuracy.

--- experiments/ultrasonic_test.py
+++ experiments/ultrasonic_test.py
@@ -7,30 +7,61 @@ import pigpio
 GPIO_TRIGGER = 14
 GPIO_ECHO = 15
 
-def read_ultrasonic():
-    pigpio.gpio_trigger(GPIO_TRIGGER)
-    stop = start = time.time()
-    while pigpio.read(GPIO_ECHO) == 0:
-        start = time.time()
-    while pigpio.read(GPIO_ECHO) == 1:
-        stop = time.time()
-    elapsed = stop - start
-    distance = elapsed * 34000
-    distance = distance / 2
-    return distance    
+LAST_ECHO = 0
+ECHO_DURATION = 0
+
+class Ultrasonic(object):
+    GPIO_TRIGGER = 14
+    GPIO_ECHO = 15
+
+    def __init__(self, decay=0.5):
+        self.decay = decay
+        self.tick_up = 0
+        self.distance = None
+        self.echo_cb = None
+
+    def start(self):
+        pigpio.set_mode(self.GPIO_TRIGGER, pigpio.OUTPUT)
+        pigpio.set_mode(self.GPIO_ECHO, pigpio.INPUT)
+        pigpio.write(self.GPIO_TRIGGER, 0)
+        self.echo_cb = pigpio.callback(
+            self.GPIO_ECHO, pigpio.EITHER_EDGE, self.on_echo)
+        pigpio.set_watchdog(self.GPIO_ECHO, 100)
+
+    def stop(self):
+        if self.echo_cb is None: return
+        pigpio.set_watchdog(self.GPIO_ECHO, 0)
+        self.echo_cb.cancel()
+        self.echo_cb = None
+
+    def update_distance(self, d1):
+        if self.distance is None:
+            self.distance = d1
+            return
+        d = self.distance
+        alpha = self.decay
+        self.distance = (alpha * d) + ((1 - alpha) * d1)
+
+    def on_echo(self, _, level, tick):
+        if level == 2:
+            pigpio.gpio_trigger(self.GPIO_TRIGGER)
+        if level == 1:
+            self.tick_up = tick
+        elif level == 0:
+            delta = pigpio.tickDiff(self.tick_up, tick)
+            self.update_distance(delta * 0.017)
 
 def main(argv=sys.argv):
     pigpio.stop()
     time.sleep(0.5)
     pigpio.start()
 
-    pigpio.set_mode(GPIO_TRIGGER, pigpio.OUTPUT)
-    pigpio.set_mode(GPIO_ECHO, pigpio.INPUT)
-    pigpio.write(GPIO_TRIGGER, 0)
-    time.sleep(0.5)
+    us = Ultrasonic()
+    us.start()
 
-    distance = read_ultrasonic()
-    print "Distance : %.1f" % (distance,)
+    while True:
+        time.sleep(1)
+        print "Distance : %.1f" % (us.distance,)
 
     pigpio.stop()
     return 0
